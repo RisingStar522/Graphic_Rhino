619ab4c6505eec9fb4b51796f76db456856c5b3d
Zeus <84690417+darkrut22@users.noreply.github.com>
2022-01-25 01:14:57 +0900

    structure

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/wrappers/App';
+import "normalize.css";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
 
-// Add bootstrap
-import 'bootstrap/dist/css/bootstrap.css';
+delete L.Icon.Default.prototype._getIconUrl;
 
-// Add our style
-import './assets/style/index.css';
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
+  iconUrl: require("leaflet/dist/images/marker-icon.png"),
+  shadowUrl: require("leaflet/dist/images/marker-shadow.png"),
+});
 
 ReactDOM.render(
-    <App/>,
-    document.getElementById('root')
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
 );
